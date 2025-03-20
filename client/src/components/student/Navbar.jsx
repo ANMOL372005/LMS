ec775117262519e4e2da7d2cd57f3e266699bbd2
AnshDabra27 <kambojansh2004@gmail.com>
2025-03-20 19:27:16 +0530

    Landing page

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,21 +1,51 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {assets} from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import {useClerk, UserButton , useUser} from '@clerk/clerk-react'
+import { AppContext } from '../../context/AppContext'
+
 const Navbar = () => {
 
-    const isCourseListPage = location.pathname.includes('/course-list')
+    const isCourseListPage = location.pathname.includes('/course-list');
+
+    const {openSignIn} = useClerk()
+    const {user} = useUser()
+
+    const {navigate,isEducator} = useContext(AppContext)
 
   return (
     <div className= {`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'} `}>
-        <img src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
+        <img onClick={()=> navigate('/')} src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
         <div className='hidden md:flex items-center gap-5 text-gray-500'>
-            <div>
-                <button>Become Educator</button>
-                <Link to='/my-enrollments'>My Enrollments</Link>
+            <div className='flex items-center gap-5'>
+                { user && <>
+                  
+                  <button onClick={()=>{navigate('/educator')}} className='cursor-pointer'>{isEducator ? 'Educator Dashboard':'Become Educator'}</button> 
+                |  <Link to='/my-enrollments'>My Enrollments</Link>
+                </>
+                }
             </div>
-            <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+            { user ? <UserButton/> : 
+              
+              <button onClick={()=>openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>}
         </div>
-        <div></div>
+        {/* For Phone Screen   */}
+        <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+            <div className='flex items-center gap-1 sm:gap-2 sm:gap-5 max-sm:text-x5'>
+            { user && <>
+
+              <button onClick={()=>{navigate('/educaotr')}} className='cursor-pointer'>{isEducator ? 'Educator Dashboard':'Become Educator'}</button>  
+              |  <Link to='/my-enrollments'>My Enrollments</Link>
+              </>
+            }
+            </div>
+            {
+              user ? <UserButton/> : <button onClick={()=>openSignIn()}><img src={assets.user_icon} alt="" /></button>
+            }
+            
+
+        </div>
+
     </div>
   )
 }
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,11 +1,43 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import React from "react";
+import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 export const AppContext = createContext();
 
 export const AppContextProvider = (props)=>{
 
-    const values = {
+    const currency = import.meta.env.VITE_CURRENCY
+    const navigate = useNavigate();
 
+   
+
+    const [allCourses , setAllCourses] = useState([])
+    const [isEducator , setIsEducator] = useState(true)
+
+    // Fetch all Courses
+    const fetchAllCourses = async ()=>{
+        setAllCourses(dummyCourses)
+    }
+
+    // Function to calculate average rating of course
+    const calculateRating = (course)=>{
+        if(course.courseRatings.length === 0){
+            return 0;
+        }
+        let totalRating = 0
+        course.courseRatings.forEach(rating => {
+            totalRating += rating.rating
+        })
+        return totalRating / course.courseRatings.length
+    }
+
+
+    useEffect(()=>{
+        fetchAllCourses()
+    },[])
+
+    const values = {
+        currency, allCourses ,navigate,calculateRating,isEducator,setIsEducator
     }
 
     return (
